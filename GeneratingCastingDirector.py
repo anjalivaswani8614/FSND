a4ef9e706e8746c9bba23ff62a2b1887089e0f34
anjalivaswani8614 <anjali.vaswani86@gmail.com>
2023-02-01 18:14:40 +0530

    testing deployment

--- GeneratingCastingDirector.py
+++ GeneratingCastingDirector.py
@@ -2,7 +2,7 @@ import http.client
 
 conn = http.client.HTTPSConnection("dev-4k4vtcp740xghw0w.us.auth0.com")
 
-payload = "{\"client_id\":\"oRW1PrlT161eJMdzeOz12hRAkpUwdR3E\",\"client_secret\":\"SVhd4gq_uafzu0-ohC_FvJVPNH_cRyItsGaQfGiaWuzO2jnRK3ako6fI_FOrJMGs\",\"audience\":\"/movies\",\"grant_type\":\"Casting Assistant\"}"
+payload = "{\"client_id\":\"oRW1PrlT161eJMdzeOz12hRAkpUwdR3E\",\"client_secret\":\"SVhd4gq_uafzu0-ohC_FvJVPNH_cRyItsGaQfGiaWuzO2jnRK3ako6fI_FOrJMGs\",\"audience\":\"/movies\",\"grant_type\":\"client_credentials\"}"
 
 headers = { 'content-type': "application/json" }
 
